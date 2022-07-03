6114d5b71a9dcbedf9d64ecded4323b46732e88c
hermitex <mercytibs@yahoo.com>
2022-07-03 12:33:12 +0300

    improve responsiveness

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -63,39 +63,5 @@ function Widget({ data, title }) {
   );
 }
 
-function Chart({ data, title, aspect }) {
-  let chartData = createChartData(data, title);
-  // console.log(chartData);
-  return (
-    <div>
-      <ResponsiveContainer width="100%" aspect={aspect}>
-        <AreaChart
-          width={830}
-          height={250}
-          data={chartData}
-          margin={{ top: 10, right: 10, left: 0, bottom: 0 }}
-        >
-          <defs>
-            <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <XAxis dataKey="time" stroke="gray" />
-          <YAxis dataKey={title} stroke="gray" />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Area
-            type="monotone"
-            dataKey={title}
-            stroke="#8884d8"
-            fillOpacity={1}
-            fill="url(#total)"
-          />
-        </AreaChart>
-      </ResponsiveContainer>
-    </div>
-  );
-}
 
 export default Widget;
